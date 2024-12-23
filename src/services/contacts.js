234a636ae5437a9132188a0da8151750974d278d
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-12-23 21:31:21 +0200

    fix/return contacts

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,11 @@
 import Contact from "../db/models/contacts.js";
 
-export const getContacts = () => {
-  Contact.find();
+export const getContacts = async () => {
+  const contacts = await Contact.find();
+  return contacts;
 };
 
-export const getContactById = (contactId) => {
-  Contact.findById(contactId);
+export const getContactById = async (contactId) => {
+  const contact = await Contact.findById(contactId);
+  return contact;
 };
